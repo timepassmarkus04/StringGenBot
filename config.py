96ddf8534065bdca5e33e622766ce90d6ed7c9bb
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-09-11 16:19:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://devilkingp0404:HpFatXEgKjMUtxkW@cluster0.3yabl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 OWNER_ID = int(getenv("OWNER_ID", 6813000899))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TSS_Support_Group")
